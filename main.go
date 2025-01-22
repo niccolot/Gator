9bf90b9bdebabfaebd5dbfd7e25231e859f7dd0f
niccolot <nico_tiezzi98hotmail.it>
2025-01-22 10:08:54 -0600

    removed commented main

--- main.go
+++ main.go
@@ -15,50 +15,6 @@ import (
 	"github.com/peterh/liner"
 )
 
-/*
-func main() {
-	errEnv := godotenv.Load()
-	if errEnv != nil {
-		log.Fatalf(fmt.Sprintf("error loading environment variables: %v", errEnv))
-	}
-
-	dbURL := os.Getenv("DB_URL")
-	db, errDB := sql.Open("postgres", dbURL)
-	if errDB != nil {
-		log.Fatalf(fmt.Sprintf("error creating APIconfig: %v", errDB))
-	}
-
-	defer db.Close()
-
-	dbQueries := database.New(db)
-
-	cfg := config.Read()
-
-	s := state.State{
-		Db: dbQueries,
-		Cfg: cfg,
-	}
-
-	cmds := commands.Commands{}
-	cmds.Init()
-
-	cliArgs := os.Args
-	if len(cliArgs) < 2 {
-		log.Fatal("enter a valid command")
-	}
-
-	cmd := commands.Command{
-		CmdName: cliArgs[1],
-		Args: cliArgs[2:],
-	}
-
-	errCmd := cmds.Run(&s, cmd)
-	if errCmd != nil {
-		log.Fatal(errCmd)
-	}
-}
-*/
-
 func main() {
 	errEnv := godotenv.Load()
 	if errEnv != nil {
